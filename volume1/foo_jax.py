842cf14ccab9f36f4f4494341313081702a58306
oceanumeric <numerical.ocean@gmail.com>
2022-12-03 21:32:29 +0100

    [organize] repo

--- volume1/foo_jax.py
+++ volume1/foo_jax.py
@@ -2,7 +2,11 @@ import jax.numpy as jnp
 from jax import grad, jit, vmap
 from jax import random
 import os
+import sys
 
+
+# check executable path
+print(sys.executable)
 # set memory allocation 
 # https://jax.readthedocs.io/en/latest/gpu_memory_allocation.html
 import os
@@ -16,3 +20,14 @@ print(x)
 size = 100
 x = random.normal(key, (size, size), dtype=jnp.float32)
 print(jnp.dot(x, x.T).block_until_ready()) # runs on the GPU
+
+
+import torch
+
+print(torch.cuda.is_available())
+
+print(torch.cuda.get_device_properties(0).total_memory)
+
+print(torch.cuda.mem_get_info(0))
+
+
